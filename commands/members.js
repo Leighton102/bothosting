2a68d84c1db2693b7f91fa9e2dc41633fb4a6477
Leighton102 <76919670+Leighton102@users.noreply.github.com>
2021-01-21 15:52:17 -0500

    zxczxczxczxczxczxczxczxczxczxczxczxczxczxc

---
+++ commands/members.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'members',
+    description: "this is the members command!",
+    execute(message, args) {
+        message.channel.send(`**${message.guild.name}** has **${message.guild.memberCount} members!**`)
+
+        console.log(`${message.author.username} executed command members in ${message.guild.name}`)
+    }
+}
