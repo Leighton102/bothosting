280f75125407966d0cc278b2141e1674fcd1d78f
Leighton102 <76919670+Leighton102@users.noreply.github.com>
2021-01-21 20:48:06 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,22 @@
+const { MessageFlags } = require("discord.js")
+
 module.exports = {
-    name: 'help',
-    description: "The help command!",
-    execute(message, args){
-        message.channel.send(`I sent you a DM! ${message.author}`)
-        message.author.send(`**Join this server for help and commands!** https://discord.gg/9BdGSUPHja`)
-        
-        console.log(`[${message.author.id}] ${message.author.username} executed command help in ${message.guild.name}`)
+    name: 'rules',
+    despription: "Shows the rules",
+    execute(message, args, Discord){
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor('#5ff9f7')
+        .setTitle('Help')
+        .setURL('https://discord.gg/9BdGSUPHja%22')
+        .setDescription('If you need help or support, join our bot server!')
+        .addFields(
+            {name: 'How to join the server!', value: 'Click on "Rules" at the top to join the server!'}
+        )
+        .setImage('https://i.imgur.com/ZOKp8LH.png')
+        .setFooter('We hope this will help you!');
+
+        message.author.send(newEmbed);
     }
+
+
 }
