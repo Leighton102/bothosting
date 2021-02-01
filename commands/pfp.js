73ced6996680f2649ae6d2073dabb4ba8511d61d
Leighton102 <76919670+Leighton102@users.noreply.github.com>
2021-02-01 14:36:24 -0500

    Update pfp.js

--- commands/pfp.js
+++ commands/pfp.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'pfp',
     description: "The pfp command!",
     execute(message, args){
-        message.channel.send(message.author.displayAvatarURL)
+        message.channel.send(`${message.author.displayAvatarURL}`)
 
         console.log(`[${message.author.id}] ${message.author.username} executed command cheese in ${message.guild.name}`)
     }
