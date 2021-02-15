b0880ae724adecb73ec2ecf5f493c55a87efdb25
Leighton102 <76919670+Leighton102@users.noreply.github.com>
2021-02-15 14:22:22 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,25 @@
+const { MessageFlags } = require("discord.js")
+
 module.exports = {
     name: 'help',
     description: "The help command!",
     execute(message, args){
-        message.channel.send(`I sent you a DM! ${message.author}`)
-        message.author.send(`**Join this server for help and commands!** https://discord.gg/9BdGSUPHja`)
-        
-        console.log(`[${message.author.id}] ${message.author.username} executed command help in ${message.guild.name}`)
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor('#3A46F0')
+        .setTitle('Bot Help')
+        .setURL('https://discord.gg/uKQPYFfmZJ')
+        .setDescription('This is an embed for the server rules')
+        .addFields(
+            {name: 'About', value: 'Welcome! This will help you with bot commands!'},
+            {name: 'Invite Link', value: 'https://discord.gg/uKQPYFfmZJ'},
+        )
+        .setImage('https://i.kym-cdn.com/entries/icons/original/000/034/772/Untitled-1.png%27')
+        .setFooter('Last Updated 2/15/2021');
+
+        message.author.send(newEmbed);
     }
+
+
 }
+        
+console.log(`[${message.author.id}] ${message.author.username} executed command help in ${message.guild.name}`)
