1ab30a736b84f5a40a4c121bbdf12637b4b4fb11
Leighton102 <76919670+Leighton102@users.noreply.github.com>
2021-02-15 14:44:24 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,22 +3,22 @@ const { MessageFlags } = require("discord.js")
 module.exports = {
     name: 'help',
     description: "The help command!",
-    execute(message, args){
+    execute(message, args) {
         const newEmbed = new Discord.MessageEmbed()
-        .setColor('#3A46F0')
-        .setTitle('Bot Help')
-        .setURL('https://discord.gg/uKQPYFfmZJ')
-        .setDescription('This is an embed for the server rules')
-        .addFields(
-            {name: 'About', value: 'Welcome! This will help you with bot commands!'},
-            {name: 'Invite Link', value: 'https://discord.gg/uKQPYFfmZJ'},
-        )
-        .setImage('https://i.kym-cdn.com/entries/icons/original/000/034/772/Untitled-1.png%27')
-        .setFooter('Last Updated 2/15/2021');
+            .setColor('#3A46F0')
+            .setTitle('Bot Help')
+            .setURL('https://discord.gg/uKQPYFfmZJ')
+            .setDescription('ThonkerBot Help & Support')
+            .addFields(
+                { name: 'About', value: 'Welcome! This will help you with bot commands!' },
+                { name: 'Invite Link', value: 'https://discord.gg/uKQPYFfmZJ' }
+            )
+            .setImage('https://i.kym-cdn.com/entries/icons/original/000/034/772/Untitled-1.png%27')
+            .setFooter('Last Updated 2/15/2021');
 
         message.channel.send(newEmbed);
     }
 
 
 }
-        
+
